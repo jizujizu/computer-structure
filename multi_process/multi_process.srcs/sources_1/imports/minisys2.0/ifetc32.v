d61c19c1167d81692d43f33a30ca7bd8717a112c
Alex-ChaoBo <59268835+Alex-ChaoBo@users.noreply.github.com>
2019-12-28 16:20:16 +0800

    写完ifetch
    通过wpc修改的不是pc，而是next_PC，不知道对不对

--- multi_process/multi_process.srcs/sources_1/imports/minisys2.0/ifetc32.v
+++ multi_process/multi_process.srcs/sources_1/imports/minisys2.0/ifetc32.v
@@ -1,73 +1,16 @@
-//`timescale 1ns / 1ps
-////////////////////////////////////////////////////////////////////////////////////
-//module Ifetc32(Instruction,PC_plus_4_out,Add_result,Read_data_1,Branch,nBranch,Jmp,Jal,Jrn,Zero,clock,reset,opcplus4);
-//    output[31:0] Instruction;			// 输出指令到其他模块
-//    output[31:0] PC_plus_4_out;         // (pc+4)送执行单元
-//    input[31:0]  Add_result;            // 来自执行单元,算出的跳转地址
-//    input[31:0]  Read_data_1;           // 来自译码单元，jr指令用的地址
-//    input        Branch;                // 来自控制单元
-//    input        nBranch;               // 来自控制单元
-//    input        Jmp;                   // 来自控制单元
-//    input        Jal;                   // 来自控制单元
-//    input        Jrn;                   // 来自控制单元
-//    input        Zero;                  //来自执行单元
-//    input        clock,reset;           //时钟与复位
-//    output[31:0] opcplus4;              // JAL指令专用的PC+4
-    
-    
-//    wire[31:0]   PC_plus_4;             // PC+4
-//    reg[31:0]	  PC;                   // PC寄存器（程序计数器）
-//    reg[31:0]    next_PC;               // 下条指令的PC（不一定是PC+4)
-//    reg[31:0]    opcplus4;
-    
-//   //分配64KB ROM，编译器实际只用 64KB ROM
-//    prgrom instmem(
-//        .clka(clock),         // input wire clka
-//        .addra(PC[15:2]),     // input wire [13 : 0] addra
-//        .douta(Instruction)         // output wire [31 : 0] douta
-//    );
-    
-
-//    assign PC_plus_4[31:2] = PC[31:2]+1'b1;
-//    assign PC_plus_4[1:0] = 2'b00;
-//    assign PC_plus_4_out = PC_plus_4[31:0];
-//    // beq $n ,$m if $n=$m branch   bne if $n /=$m branch jr
-//    always @* begin  
-//        if ((Branch & Zero) | (nBranch & ~Zero)) next_PC = Add_result;
-//        else if (Jrn) next_PC = Read_data_1;
-//        else next_PC = PC_plus_4[31:2];
-//    end
-//    //????J??Jal????reset????? 
-//    always@(negedge clock) begin  
-//        if (reset) begin
-//            PC = 32'b0;
-//            next_PC = 32'b1;
-//        end else begin
-//            if (Jmp) PC = opcplus4;
-//            else if (Jal) begin
-//                PC[31:28] = PC_plus_4_out[31:28];
-//                PC[27:2] = Instruction[25:0];
-//                PC[1:0] = 2'b0;
-//                opcplus4 = next_PC;
-//            end
-//            else 
-//                PC[31:2] = next_PC;
-//        end
-//    end
-//endmodule
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-module Ifetc32(Instruction,PC_plus_4_out,Add_result,Read_data_1,Branch,nBranch,Jmp,Jal,Jrn,Zero,clock,reset,opcplus4);
+module Ifetc32(Instruction,PC_plus_4_out,Add_result,Read_data_1,Jmp,Jal,clock,reset,opcplus4, Wpc, Wir);
     output[31:0] Instruction;			// 输出指令到其他模块
     output[31:0] PC_plus_4_out;         // (pc+4)送执行单元
     input[31:0]  Add_result;            // 来自执行单元,算出的跳转地址
     input[31:0]  Read_data_1;           // 来自译码单元，jr指令用的地址
-    input        Branch;                // 来自控制单元
-    input        nBranch;               // 来自控制单元
+    //input        Branch;                // 来自控制单元
+    //input        nBranch;               // 来自控制单元
     input        Jmp;                   // 来自控制单元
     input        Jal;                   // 来自控制单元
-    input        Jrn;                   // 来自控制单元
-    input        Zero;                  //来自执行单元
+    //input        Jrn;                   // 来自控制单元
+    //input        Zero;                  //来自执行单元
     input        clock,reset;           //时钟与复位
     output[31:0] opcplus4;              // JAL指令专用的PC+4
     
@@ -77,24 +20,45 @@ module Ifetc32(Instruction,PC_plus_4_out,Add_result,Read_data_1,Branch,nBranch,J
     reg[31:0]    next_PC;               // 下条指令的PC（不一定是PC+4)
     reg[31:0]    opcplus4;
     
+    // multi process begin
+    input[1:0] Wpc;
+    input Wir;
+    wire[31:0] ins;
+    
+    always@(negedge clock) begin 
+        if(reset)begin 
+            IR<=0;
+        end else if(Wir) begin 
+            IR<=ins;
+        end else begin 
+            IR<=IR;
+        end 
+    end
+    
+    assign Instruction=IR;
+    
+    // multi process end
+    
+    
+    
    //分配64KB ROM，编译器实际只用 64KB ROM
     prgrom instmem(
         .clka(clock),         // input wire clka
         .addra(PC[15:2]),     // input wire [13 : 0] addra
-        .douta(Instruction)         // output wire [31 : 0] douta
+        .douta(ins)         // output wire [31 : 0] douta
     );
-    
+
     assign PC_plus_4[31:2] = PC[31:2]+1'b1;     //  此处＋1实际上是＋4，因为这里低2位始终为00，＋1加在D2位上
     assign PC_plus_4[1:0] = 2'b00;
-    assign PC_plus_4_out = PC_plus_4[31:0];  //  PC＋4送到执行单元，以便执行单元在必要的时候算出ADDRESULT
+    assign PC_plus_4_out = IR;  //  PC＋4送到执行单元，以便执行单元在必要的时候算出ADDRESULT
 
-    always @* begin                          // beq $n ,$m if $n=$m branch   bne if $n /=$m branch
-        if(((Branch == 1) && (Zero == 1 )) || ((nBranch == 1) && (Zero == 0)))
-            next_PC = Add_result;           //  计算出的新PC地址
-        else if(Jrn == 1)
-            next_PC = Read_data_1[31:0];
-        else  next_PC = {2'b00,PC_plus_4[31:2]};// 其他时候都是PC<-PC+4
-    end
+//    always @* begin                          // beq $n ,$m if $n=$m branch   bne if $n /=$m branch
+//        if(((Branch == 1) && (Zero == 1 )) || ((nBranch == 1) && (Zero == 0)))
+//            next_PC = Add_result;           //  计算出的新PC地址
+//        else if(Jrn == 1)
+//            next_PC = Read_data_1[31:0];
+//        else  next_PC = {2'b00,PC_plus_4[31:2]};// 其他时候都是PC<-PC+4
+//    end
     
    always @(negedge clock) begin
      if(reset == 1) begin
@@ -106,4 +70,17 @@ module Ifetc32(Instruction,PC_plus_4_out,Add_result,Read_data_1,Branch,nBranch,J
        end else PC[31:0] <= {next_PC[29:0],2'b00};
      end
    end
+   
+    always@(negedge clock) begin 
+        if(reset)begin 
+            next_PC<=32'b0;
+        end else begin 
+            case(Wpc)
+                2'b01: next_PC<={2'b00,PC_plus_4[31:2]};
+                2'b10: next_PC<=Read_data_1[31:0];
+                2'b11: next_PC = Add_result;
+            endcase
+        end 
+    end
+   
 endmodule
